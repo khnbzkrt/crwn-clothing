a48ca192c777c71b52bbbe10b06e7fca4d61a521
khnbzkrt <khan.bozkurt@gmail.com>
2021-04-23 14:47:41 +0300

    style değişiklikleri

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -28,16 +28,22 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem}></CheckoutItem>
-    ))}
-    <div className="total">TOTAL : ${total}</div>
-    <div className="test-warning">
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - Exp: 01/22 - CVV : 123
-    </div>
-    <StripeCheckoutButton price={total} />
+    {cartItems.length > 0 ? (
+      <>
+        {cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem}></CheckoutItem>
+        ))}
+        <div className="total">TOTAL : ${total}</div>
+        <div className="test-warning">
+          *Please use the following test credit card for payments*
+          <br />
+          4242 4242 4242 4242 - Exp: 01/22 - CVV : 123
+        </div>
+        <StripeCheckoutButton price={total} />
+      </>
+    ) : (
+      <div className="isempty">Your checkout is empty</div>
+    )}
   </div>
 );
 
